b1af819e16c632c9d5de29cb88e5afb09639c9c7
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-01-14 19:18:47 +0300

    Update message controllers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,54 +4,44 @@ class MessagesController < ApplicationController
   skip_before_action :authorized, only: [:index]
 
   #GET
-    def index 
-    if params[:customer_id], params[:handyman_id]
-    messages = Message.find(params[:customer_id], params[:handyman_id])
-    else
-        messages = Message.all
-        render json: messages
-    end
+  def index
+    messages = Message.all
+    render json: messages, status: :ok
+  end
 
-    def show
-    #     message =  Message.find(params[:customer_id], params[:handymen_id])
-    #     if message
-    #         render json: message
-    #     else
-    #         render json:{ error: "message not found"}, status: :not_found
-    # end
+  def show
     message = Message.find(params[:id])
     render json: message, status: :ok
-end
+  end
 
-#POST
-def create 
-    if params[:customer_id], params[:handyman_id]
+  #POST
+  def create
     message = Message.create!(message_params)
-    render json:message, status: :created
-end
+    render json: message, status: :created
+  end
 
-
-#PUT/UPDATE
-def update 
+  #PUT/UPDATE
+  def update
     message = Message.update!(message_params)
     render json: message, status: :accepted
-end
+  end
 
-#DELETE
-def destroy
+  #DELETE
+  def destroy
     message = find_message
     message.destroy
     head :no_content
-end
+  end
 
-private
+  private
+
   def message_params
-    params.permit(:customer_id, :handyman_id, :message )
-  end 
+    params.permit(:customer_id, :handyman_id, :message)
+  end
 
   def find_message
     Message.find(params[:id])
-   end
+  end
 
   def render_message_not_found_response
     render json: { errors: ["Message not found"] }, status: :not_found
@@ -60,5 +50,4 @@ private
   def render_unprocessable_entity(invalid)
     render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
-  
 end
