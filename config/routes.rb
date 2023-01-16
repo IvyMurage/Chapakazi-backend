1cca42451b1f11647d62be858500e3a4b80198fd
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-01-16 19:19:15 +0300

    Add a login route for admin

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
 
   post "handyman/login", to: "handymenauth#create"
   post "customer/login", to: "customersauth#create"
+  post "admin/login", to: "adminsauth#create"
   patch "customer/password-reset", to: "customers#update"
   patch "handyman/password-reset", to: "handymen#update"
   patch "/reviews/:id/like", to: "reviews#increment_likes"
   patch "/reviews/:id/dislike", to: "reviews#decrement_likes"
+
   # patch "handymen/:id/image", to: "handymen#update_image"
 end
